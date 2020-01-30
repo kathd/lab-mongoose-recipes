ad3bab04e8baca033e7db7bbb106c5d0014cea16
Kathleen Domingo <kathleensdomingo@gmail.com>
2020-01-30 08:41:58 +0100

    iterations done, without considering asynchronousity :D

--- index.js
+++ index.js
@@ -11,3 +11,48 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+
+// Iteration 2
+  Recipe.create(data)
+  .then(recipes => {
+    recipes.forEach(recipe => {
+      console.log(recipe.title);
+    })
+  })
+  .catch(err => console.error("An error happened while saving a new recipe:", err));
+
+
+// Iteration 3
+  Recipe.insertMany(data)
+    .then(recipes => console.log(recipes))
+    .catch(err => console.error("An error happened while saving recipes", err))
+
+
+// Iteration 4 
+  Recipe.findByIdAndUpdate("5e31a47d749af4fa6d1e2530", {duration: 100}, {new: true})
+    .then(res => {
+      console.log("SUCCESS: recipe updated", res);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+
+
+// Iteration 5
+  Recipe.deleteOne({title: "Carrot Cake"})
+  .then(res => {
+    console.log("SUCCESS: recipe deleted", res);
+  })
+  .catch(err => {
+    console.error(err);
+  })
+
+  
+// Iteration 6
+  mongoose.connection.close()
+    .then(res => console.log("Connection Closed", res))
+    .catch(err => console.error(err))
+
+
+
